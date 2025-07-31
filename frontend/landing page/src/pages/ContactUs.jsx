a1374a74f8062bcf61a0ab38fb70c9e4e2c16495
GabrielOmokaro <emekaomokaro54@gmail.com>
2025-07-31 22:59:11 +0100

    my contact page

--- frontend/landing page/src/pages/ContactUs.jsx
+++ frontend/landing page/src/pages/ContactUs.jsx
@@ -1,7 +1,34 @@
+import Navbar from "../ui/Navbar";
+import Footer from "../ui/Footer";
+import ContactHero from "../ui/ContactHero";
+import ContactSection from "../ui/ContactSection";
+import ContactLink from "../ui/ContactLink";
+
 const ContactUs = () => {
   return (
     <div>
-      <h1>ContactUs page</h1>
+      {/* Navbar */}
+      <Navbar />
+      {/* Hero Section */}
+      {/* <div className="img-text">
+        <img
+          src="../../public/HeroSec.png"
+          alt="HeroSec.png"
+          className="w-full h-[400px] object-cover"
+        />
+
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
+          <h1 className="text-4xl font-bold mb-24 text-green-600 mb-24 text-fixed ">
+            Contact <span className="text-white">ECOTRACK</span>
+          </h1>
+        </div>
+      </div> */}
+      <ContactHero />
+      <ContactSection />
+      <ContactLink />
+
+     
+      <Footer />
     </div>
   );
 };
